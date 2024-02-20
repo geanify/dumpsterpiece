5f32045c46779ea8010eb5cff73a32de5644e63f
geanify <geanify@gmail.com>
2024-02-20 14:35:56 +0200

    remove useless line

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const useMemePad = (a,b,c='') => ((str, len, char = ' ') => `${[...new Array(len - str.length)].fill(char).join('')}${str}`)(a+'', b+'', (c+'')[0])
 
-
 const useBinarySearch = (arr = [], nr, l=0, h=arr.length) => arr[Math.floor(l + (h-l)/2)] === nr ? Math.floor(l + (h-l)/2) : (l >= h ? -1 : (nr <= arr[Math.floor(l + (h-l)/2)] ? useBinarySearch(arr, nr, l, Math.floor(l + (h-l)/2)) : useBinarySearch(arr, nr, Math.floor(l + (h-l)/2), h))) 
 
 
